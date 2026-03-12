3924bf445591072b38dab7a27dd6bf9ebea143e2
Anik <anik.khondokar09@gmail.com>
2026-03-12 09:11:20 +0600

    add money event listener added

---
+++ home.js
@@ -0,0 +1,28 @@
+// add money section
+document
+  .getElementById("Add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    //dataBase information
+    const userAccountNumber='01776433215'
+    const pass="anik"
+
+    // get the balance
+    const balance = parseInt(
+      document.getElementById("total-balance").innerText,
+    );
+    // get the input info
+    const accountNumber = document.getElementById("account-number").value
+    const amount = parseInt(document.getElementById("add-amount").value);
+    const password = document.getElementById("pass-word").value;
+
+    if( accountNumber===userAccountNumber && password===pass){
+       document.getElementById('total-balance').innerText= balance+amount;
+        alert('Add Money successful')
+    }else{
+        alert("Invalid Information!")
+    }
+  });
+
+  
