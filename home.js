a3b808a281f26a1cdbaffe96dad81654fd8c87b9
Anik <anik.khondokar09@gmail.com>
2026-03-13 09:37:31 +0600

    added transaction feature

--- home.js
+++ home.js
@@ -1,3 +1,12 @@
+const transaction=[]
+function addTransacton(data){
+  const transaction={
+    name:data,
+    date:new Date().toLocaleDateString()
+  }
+  return transaction;
+}
+
 // add money section
 document
   .getElementById("Add-money-btn")
@@ -23,6 +32,9 @@ document
     } else {
       alert("Invalid Information!");
     }
+
+    //adding transaction to the card
+    transaction.push(addTransacton("Add Money"));
   });
 
 // withdraw out section
@@ -36,43 +48,73 @@ document.getElementById("w-btn").addEventListener("click", function (e) {
   const amount = parseInt(document.getElementById("cash-out-amount").value);
   const password = document.getElementById("pin-number").value;
 
- if(password===pass){
-    document.getElementById("total-balance").innerText-=amount;
-    alert('Cash out is Done!');
- }else{
+  if (password === pass) {
+    document.getElementById("total-balance").innerText -= amount;
+    alert("Cash out is Done!");
+  } else {
     alert("Invalid credential!");
- }
-
+  }
+  
+  //adding transaction to the card
+  transaction.push(addTransacton("Withdraw Money"))
 });
+
+//transaction section event
+document.getElementById("Transactions-card").addEventListener('click',function(){
+  const tranctionContainer = document.getElementById("onkNamDisi");
+  tranctionContainer.innerText=''
+
+  for(const card of transaction){
+    const div=document.createElement('div')
+    div.innerHTML = `
+    <div class="flex justify-between items-center my-3 shadow-md p-4">
+          <div class="flex justify-items-start gap-4 items-center">
+            <div>
+              <img src="./assets/purse1.png" alt=""></div>
+            <div>
+              <p class="font-semibold text-lg">${card.name}</p>
+              <p class="text-gray-500">${card.date}</p>
+            </div>
+          </div>
+          <!-- icon -->
+           <i class="fa-solid fa-ellipsis rotate-90"></i>
+        </div>
+    `;
+    tranctionContainer.appendChild(div)
+  }
+
+})
 
 //   toggleing feature
 function toggleWithId(cardId, sectionClass, sectionId) {
-  document.getElementById(cardId).addEventListener('click', function (e) {
+  document.getElementById(cardId).addEventListener("click", function (e) {
     // Hide all sections with the given class
-    e.preventDefault()
+    e.preventDefault();
     const sections = document.getElementsByClassName(sectionClass);
     for (const section of sections) {
-      section.style.display = 'none';
+      section.style.display = "none";
     }
 
-    const common2=document.getElementsByClassName('common2');
-    for(const common of common2){
+    const common2 = document.getElementsByClassName("common2");
+    for (const common of common2) {
       common.classList.remove("border-green-500", "border-2");
     }
 
     // Show the selected section
     const activeSection = document.getElementById(sectionId);
-    activeSection.style.display = 'block';
-    document.getElementById(cardId).classList.add("border-green-500", "border-2");
+    activeSection.style.display = "block";
+    document
+      .getElementById(cardId)
+      .classList.add("border-green-500", "border-2");
   });
 }
 
-toggleWithId('add-money-card', 'common', 'add-money');
-toggleWithId('cash-out-card', 'common', 'cash-out');
-toggleWithId("transfer-card",'common','transfer-money');
-toggleWithId("bonus-card",'common','get-bonus');
+toggleWithId("add-money-card", "common", "add-money");
+toggleWithId("cash-out-card", "common", "cash-out");
+toggleWithId("transfer-card", "common", "transfer-money");
+toggleWithId("bonus-card", "common", "get-bonus");
 toggleWithId("payBill-card", "common", "pay-bill");
-
+toggleWithId("Transactions-card", "common", "transaction-container");
 
 // document
 //   .getElementById("add-money-card")
