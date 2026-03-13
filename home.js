711290bff7aae3ee6f76569fd01863c37c34be44
Anik <anik.khondokar09@gmail.com>
2026-03-13 08:06:30 +0600

    added transfer money,get bonus and pay bill section

--- home.js
+++ home.js
@@ -46,14 +46,41 @@ document.getElementById("w-btn").addEventListener("click", function (e) {
 });
 
 //   toggleing feature
+function toggleWithId(cardId, sectionClass, sectionId) {
+  document.getElementById(cardId).addEventListener('click', function (e) {
+    // Hide all sections with the given class
+    e.preventDefault()
+    const sections = document.getElementsByClassName(sectionClass);
+    for (const section of sections) {
+      section.style.display = 'none';
+    }
 
-document
-  .getElementById("add-money-card")
-  .addEventListener("click", function () {
-    document.getElementById("add-money").style.display = "block";
-    document.getElementById("cash-out").style.display = "none";
+    const common2=document.getElementsByClassName('common2');
+    for(const common of common2){
+      common.classList.remove("border-green-500", "border-2");
+    }
+
+    // Show the selected section
+    const activeSection = document.getElementById(sectionId);
+    activeSection.style.display = 'block';
+    document.getElementById(cardId).classList.add("border-green-500", "border-2");
   });
-document.getElementById("cash-out-card").addEventListener("click", function () {
-  document.getElementById("add-money").style.display = "none";
-  document.getElementById("cash-out").style.display = "block";
-});
+}
+
+toggleWithId('add-money-card', 'common', 'add-money');
+toggleWithId('cash-out-card', 'common', 'cash-out');
+toggleWithId("transfer-card",'common','transfer-money');
+toggleWithId("bonus-card",'common','get-bonus');
+toggleWithId("payBill-card", "common", "pay-bill");
+
+
+// document
+//   .getElementById("add-money-card")
+//   .addEventListener("click", function () {
+//     document.getElementById("add-money").style.display = "block";
+//     document.getElementById("cash-out").style.display = "none";
+//   });
+// document.getElementById("cash-out-card").addEventListener("click", function () {
+//   document.getElementById("add-money").style.display = "none";
+//   document.getElementById("cash-out").style.display = "block";
+// });
